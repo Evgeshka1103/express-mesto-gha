128cde57612f0e346095c1cbe459986d37e8826f
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-01-08 01:35:33 +0300

    final

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,12 @@ const express = require('express');
 
 const usersRoutes = require('./users');
 
+const cardsRoutes = require('./cards');
+
 const routes = express.Router();
 
 routes.use('/users', usersRoutes);
+
+routes.use('/cards', cardsRoutes);
 
 module.exports = routes;
