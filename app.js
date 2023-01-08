3c22951ff73c73774823a0f3ddaa33233369c50a
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-01-08 16:31:01 +0300

    test2

--- app.js
+++ app.js
@@ -12,13 +12,7 @@ mongoose.set('strictQuery', true);
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '63b95efec71e9fd3602f5d97',
-  };
 
-  next();
-});
 
 app.use(express.json());
 
