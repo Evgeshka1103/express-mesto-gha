e499bb7f7413107ddd8f18ee5f510e95ad9afaa1
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-01-09 21:54:41 +0300

    14

--- controllers/users.js
+++ controllers/users.js
@@ -61,7 +61,13 @@ const updateProfile = (req, res) => {
       runValidators: true,
     },
   )
-    .then((user) => res.status(OK).send(user))
+    .then((user) => {
+      if (!user) {
+        res.status(NotFound).send({ message: 'Не найдено' });
+      } else {
+        res.status(OK).send(user);
+      }
+    })
     .catch((err) => {
       if (err.name === 'ValidationError') {
         res.status(BadRequest).send({ message: 'Некорректный запрос' });
