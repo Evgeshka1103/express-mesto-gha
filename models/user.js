8952663f934e32a7ed15c4381a9633816a9ff265
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-01-10 22:39:37 +0300

    avtotest

--- models/user.js
+++ models/user.js
@@ -3,21 +3,17 @@ const mongoose = require('mongoose');
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
-    default: 'Жак-Ив Кусто',
     minlength: 2,
     maxlength: 30,
     required: true,
   },
   about: {
     type: String,
-    default: 'Исследователь',
     minlength: 2,
     maxlength: 30,
     required: true,
   },
   avatar: {
-    default:
-      'https://www.rgo.ru/sites/default/files/styles/head_image_article/public/1034295-e1477344635669-1.jpg?itok=4U4Dw9en',
     type: String,
     required: true,
   },
