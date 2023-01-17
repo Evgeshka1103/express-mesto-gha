c95132ebc4849842ba8743b3a141d93a64227518
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-01-18 01:15:30 +0300

    test

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,19 +1,23 @@
 const jwt = require('jsonwebtoken');
+
 const UnauthorizedError = require('../errors/UnauthorizedError');
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
+  const { authorization } = req.headers;
 
-  if (!token) {
-    next(new UnauthorizedError({ message: 'Используйте действительную почту и пароль' }));
-    return;
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    throw new UnauthorizedError('Используйте действительную почту и пароль');
   }
+
+  const token = authorization.replace('Bearer ', '');
   let payload;
+
   try {
     payload = jwt.verify(token, 'some-secret-key');
   } catch (err) {
-    next(new UnauthorizedError({ message: 'Используйте действительную почту и пароль' }));
+    next(new UnauthorizedError('Используйте действительную почту и пароль'));
   }
+
   req.user = payload;
   next();
 };
