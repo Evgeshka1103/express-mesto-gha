4816bca0172295b86afdd88b1e35d7dd70f1e900
Evgeshka1103 <mamasoni2709@yandex.ru>
2023-01-18 15:49:24 +0300

    test

--- models/user.js
+++ models/user.js
@@ -1,7 +1,12 @@
 const mongoose = require('mongoose');
+
 const bcrypt = require('bcryptjs');
+
 const isEmail = require('validator/lib/isEmail');
+
 const isUrl = require('validator/lib/isURL');
+
+const UnauthorizedError = require('../errors/UnauthorizedError');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -47,12 +52,12 @@ userSchema.statics.findUserByCredantials = function (email, password) {
   return this.findOne({ email }).select('+password')
     .then((user) => {
       if (!user) {
-        return Promise.reject(new Error('Неправильные почта или пароль'));
+        return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
       }
       return bcrypt.compare(password, user.password)
         .then((matched) => {
           if (!matched) {
-            return Promise.reject(new Error('Неправильные почта или пароль'));
+            return Promise.reject(new UnauthorizedError('Неправильные почта или пароль'));
           }
           return user;
         });
